9766f10949722f765dd44ef7497d96663cf4a78f
CYY1007 <younguk86@gmail.com>
2023-03-31 22:14:55 +0900

    feat : POST /shared/{fileId}/member/{memberId} 시그니처

--- src/main/java/store/dropthebeatbox/app/web/controller/SharedFileRestController.java
+++ src/main/java/store/dropthebeatbox/app/web/controller/SharedFileRestController.java
@@ -1,9 +1,8 @@
 package store.dropthebeatbox.app.web.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import store.dropthebeatbox.app.web.dto.SharedFileRequestDto;
 import store.dropthebeatbox.app.web.dto.SharedFileResponseDto;
 
 @RestController
@@ -11,6 +10,11 @@ public class SharedFileRestController {
 
     @GetMapping("/shared/{fileId}/members")
     public ResponseEntity<SharedFileResponseDto.sharedMemberListDto> getMemberListByShared(@PathVariable(name = "fileId") Long fileId){
+        return null;
+    }
+
+    @PostMapping("/shared/{fileId}/member")
+    public ResponseEntity<SharedFileRequestDto.CreateSharedFileDto> createSharedFile(@PathVariable(name = "fileId")Long fileId , @RequestBody SharedFileRequestDto.CreateSharedFileDto request){
         return null;
     }
 }
