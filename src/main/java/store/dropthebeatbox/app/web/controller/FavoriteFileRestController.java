ab44c73ff5d9fcbf035ce98aa9b27604c5b8f273
CYY1007 <younguk86@gmail.com>
2023-03-31 22:02:25 +0900

    feat : GET /member/favorite 시그니처

---
+++ src/main/java/store/dropthebeatbox/app/web/controller/FavoriteFileRestController.java
@@ -0,0 +1,15 @@
+package store.dropthebeatbox.app.web.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import store.dropthebeatbox.app.web.dto.FavoriteFileResponseDto;
+
+@RestController
+public class FavoriteFileRestController {
+
+    @GetMapping("/member/favorite")
+    public ResponseEntity<FavoriteFileResponseDto.FavoriteFileDto> getFavoriteFileByMemberId(){
+        return null;
+    }
+}
---
+++ src/main/java/store/dropthebeatbox/app/web/dto/FavoriteFileResponseDto.java
@@ -0,0 +1,14 @@
+package store.dropthebeatbox.app.web.dto;
+
+import lombok.*;
+
+public class FavoriteFileResponseDto {
+
+    @Builder
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @AllArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class FavoriteFileDto{
+        String filed;
+    }
+}
