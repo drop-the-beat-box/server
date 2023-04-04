9a672d1fed8a7ca7bfd3b5daa4de920c1d41468c
yongwook <60510921+CYY1007@users.noreply.github.com>
2023-04-04 16:24:36 +0900

    "Refactor FavoriteFileRequestDto 수정"
    롬복 어노테이션 수정

--- src/main/java/store/dropthebeatbox/app/web/dto/FavoriteFileRequestDto.java
+++ src/main/java/store/dropthebeatbox/app/web/dto/FavoriteFileRequestDto.java
@@ -8,8 +8,6 @@ import lombok.NoArgsConstructor;
 public class FavoriteFileRequestDto {
 
     @Getter
-    @NoArgsConstructor(access = AccessLevel.PROTECTED)
-    @AllArgsConstructor(access = AccessLevel.PROTECTED)
     public static class CreateFavoriteFileDto{
         String filed;
     }
