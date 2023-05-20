f0b7a0bff3eb4ad035842a3f28786ea8247f3c7e
swa07016 <swa07016@naver.com>
2023-05-20 15:57:58 +0900

    feat: @ExistMember 어노테이션

---
+++ src/main/java/store/dropthebeatbox/app/validation/annotation/ExistMember.java
@@ -0,0 +1,18 @@
+package store.dropthebeatbox.app.validation.annotation;
+
+import store.dropthebeatbox.app.validation.validator.FileExistValidator;
+import store.dropthebeatbox.app.validation.validator.MemberExistValidator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = MemberExistValidator.class)
+@Target( { ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ExistMember {
+    String message() default "해당하는 사용자가 존재하지 않습니다.";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
