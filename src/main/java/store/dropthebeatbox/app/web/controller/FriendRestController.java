b0c1b03b72f696afe2512045e8dd93cc1dc42c55
CYY1007 <younguk86@gmail.com>
2023-06-04 16:14:19 +0900

    docs : Member API, Friend API, Team API 설명 추가

--- src/main/java/store/dropthebeatbox/app/web/controller/FriendRestController.java
+++ src/main/java/store/dropthebeatbox/app/web/controller/FriendRestController.java
@@ -1,5 +1,8 @@
 package store.dropthebeatbox.app.web.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -26,18 +29,25 @@ public class FriendRestController {
 
     private final FriendService friendService;
 
+    @Operation(summary = "친구목록 조회", description = "로그인 한 사용자의 친구 목록 조회입니다.")
+    @Parameters({
+            @Parameter(name = "member", hidden = true)
+        }
+    )
     @GetMapping("/member/friends")
     public ResponseEntity<FriendResponseDto.FriendListDto> getFriendList(@AuthUser Member member) {
         List<Member> memberList = friendService.findAllByMemberId(member.getId());
         return ResponseEntity.ok(FriendConverter.toFriendListDto(memberList));
     }
 
+    @Deprecated
     @GetMapping("/member/friend/requests")
     public ResponseEntity<FriendResponseDto.FriendRequestListDto> getFriendRequestList(@AuthUser Member member) {
         List<FriendRequest> fromMemberList = friendService.findAllByToMemberId(member.getId());
         return ResponseEntity.ok(FriendConverter.toFriendRequestListDto(fromMemberList));
     }
 
+    @Deprecated
     @PostMapping("/member/friend/{friendId}/request")
     public ResponseEntity<FriendResponseDto.CreateFriendRequestDto> createFriendRequest(@PathVariable(name = "friendId") @ExistMember Long friendId, @AuthUser Member member) {
         System.out.println("in friend Controller");
@@ -46,12 +56,20 @@ public class FriendRestController {
         return ResponseEntity.ok(FriendConverter.toCreateFriendRequestDto(createdFriendRequest));
     }
 
+    @Deprecated
     @PostMapping("/member/friend/request/{requestId}")
     public ResponseEntity<FriendResponseDto.AcceptFriendRequestDto> acceptFriendRequest(@PathVariable(name = "requestId") @ExistFriendRequest Long requestId, @AuthUser Member member) {
         Friend newFriend = friendService.createFriend(member, requestId);
         return ResponseEntity.ok(FriendConverter.toAcceptFriendRequestDto(newFriend));
     }
 
+    @Operation(summary = "친구 삭제입니다.", description = "삭제 하려는 친구 아이디로 삭제합니다.")
+    @Parameters(
+            {
+                    @Parameter(name = "friendId", description = "삭제 하려는 친구 아이디"),
+                    @Parameter(name = "member", hidden = true)
+            }
+    )
     @DeleteMapping("/member/friend/{friendId}")
     public ResponseEntity<FriendResponseDto.DeleteFriendDto> deleteFriend(@PathVariable(name = "friendId") @ExistMember Long friendId, @AuthUser Member member) {
         friendService.delete(friendId);
